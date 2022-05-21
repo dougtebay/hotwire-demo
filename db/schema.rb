63d60e07ca28b0bf2b6e1155750e20946d38addd
Doug Tebay <dougtebay@gmail.com>
2022-05-21 13:59:25 -0500

    Add lists migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_034452) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_181650) do
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
