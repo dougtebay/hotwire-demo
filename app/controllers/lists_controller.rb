58b2b2ce30e3637a949772f332f22b2a049b77c1
Doug Tebay <dougtebay@gmail.com>
2022-05-26 21:11:32 -0500

    Add form and create action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,10 +2,25 @@ class ListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @list = List.new
     @lists = current_user.lists
   end
 
   def show
     @list = List.find(params[:id])
+  end
+
+  def create
+    @list = current_user.lists.new(list_params)
+
+    if @list.save
+      redirect_to '/lists'
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
